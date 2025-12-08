c2f3cb3178f658b6b1a013f94e21b45fa9d50e6a
Manu Sharma <manusharma2462@gmail.com>
2025-12-08 16:18:59 +0530

    add header component and basic styling

---
+++ src/Components/Header.tsx
@@ -0,0 +1,10 @@
+export function Header() {
+  return (
+    <>
+      <div className="header">
+        <img className="chef-img" src="src/assets/chef.png" alt="chef-icon" />
+        <span>Chef Claude</span>
+      </div>
+    </>
+  );
+}
