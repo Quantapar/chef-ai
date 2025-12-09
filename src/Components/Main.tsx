d701c38517ac103140564b0e6add0699b8149c9d
Manu Sharma <manusharma2462@gmail.com>
2025-12-09 16:26:17 +0530

    added main component

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,8 +1,32 @@
+import { useState } from "react";
+import type { FormEvent } from "react";
+
 export function Main() {
+  const [ingredients, setIngredients] = useState<string[]>([]);
+
+  const ingredientsListItems = ingredients.map((ingredient) => (
+    <li key={ingredient}>{ingredient}</li>
+  ));
+
+  function handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const newIngredient = formData.get("ingredient");
+
+    if (typeof newIngredient !== "string") return;
+
+    setIngredients((prev) => [...prev, newIngredient.trim()]);
+
+    event.currentTarget.reset(); // <-- clears the input
+  }
   return (
-    <form className="add-ingredient-form ">
-      <input type="text" placeholder="e.g. apple" />
-      <button>Add ingredient</button>
-    </form>
+    <>
+      <form onSubmit={handleSubmit} className="add-ingredient-form ">
+        <input type="text" placeholder="e.g. apple" name="ingredient" />
+        <button type="submit">Add ingredient</button>
+      </form>
+      <ul className="list">{ingredientsListItems}</ul>
+    </>
   );
 }
